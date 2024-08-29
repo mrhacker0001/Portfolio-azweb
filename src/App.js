f0445218afcd863497c587ce0c40fe4fdb9b26bf
Your Name <your-email@example.com>
2024-08-29 16:24:41 +0500

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import './App.css'
+import Navbar from './Components/Navbar'
 
 function App() {
 
   return (
     <>
+      <Navbar />
     </>
   )
 }
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,9 +1,22 @@
 import React from 'react'
 import "./Navbar.css"
+import logo from "../assets/Screenshot 2024-03-07 at 21.47.46.png"
 
 function Navbar() {
   return (
-    <div className='Navbar'></div>
+    <div className='Navbar'>
+      <nav className='nav'>
+        <img src={logo} alt="..." />
+
+        <div className="tags">
+          <a href="...">Home</a>
+          <a href="...">Portfolio</a>
+          <a href="...">About me</a>
+          <a href="...">Testimonials</a>
+        </div>
+        <button>Contact me</button>
+      </nav>
+    </div>
   )
 }
 
