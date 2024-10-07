d24ed5bef29878b8bc0db8d2827d5100717dff1e
Your Name <your-email@example.com>
2024-10-07 19:14:17 +0500

    css added

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,10 +1,7 @@
 import React from 'react'
 import "./Footer.css"
 import logo from "../assets/Screenshot 2024-03-07 at 21.47.46.png"
-import facebook from "../assets/Facebook.png"
-import instagram from "../assets/Instagram.png"
-import twitter from "../assets/Twitter.png"
-import linked from "../assets/LinkedIn.png"
+import { GitHub, Instagram, Telegram } from '@mui/icons-material'
 
 function Footer() {
     return (
@@ -19,10 +16,9 @@ function Footer() {
                     <a href="...">Testimonials</a>
                 </div>
                 <div className="imgs">
-                    <img src={facebook} alt="" />
-                    <img src={instagram} alt="" />
-                    <img src={twitter} alt="" />
-                    <img src={linked} alt="" />
+                    <a href="https://www.instagram.com/16.sharifjanof?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=="><Instagram /></a>
+                    <a href="https://t.me/Azweb_off"><Telegram /></a>
+                    <a href="https://github.com/"><GitHub /></a>
                 </div>
             </div>
 
